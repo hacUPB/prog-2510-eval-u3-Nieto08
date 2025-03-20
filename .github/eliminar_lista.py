e5cf2ad0d2f62c93ec12ae7bc685296d96e9dd85
JUAN NIETO <Juanchosnieto@hotmail.com>
2025-03-20 18:04:19 -0500

    archios varios

---
+++ .github/eliminar_lista.py
@@ -0,0 +1,13 @@
+numeros = [1, 2, 3, 4, 5]
+
+# Eliminar un elemento por valor
+numeros.remove(3)
+
+# Eliminar un elemento por índice y obtener su valor
+valor_eliminado = numeros.pop(1)
+
+# Eliminar todos los elementos de la lista
+numeros.clear()
+
+print(numeros)
+print("Valor Eliminado:", valor_eliminado)
--- .github/listas.py
+++ .github/listas.py
@@ -1,7 +1,7 @@
 list_cancion=["palabra","do the evolution","siguiendo la luna","ragamuffin","parte y choke","el pichón"]
 list_artista=["sabue","pearl jam", "los fabulosos cadillacs","akapellah","jombriel","ryan castro"]
 for i in range(0,6): # Tambien se puede colocar range(6):, que se inicializa igual en 0, hasta 6-1. 
-    print(f"{list_cancion[i]} interpretada por {list_artista[i]}")
+    print(f"{list_cancion[i]} interpretada por {list_artista[i]}") #va asignando valores a i en ambos 
 for cancion in list_cancion:
     print(cancion)
 #le da valores 1 a 1 en cada interación y lo guarda en la variable canción. 
